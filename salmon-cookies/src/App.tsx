dcfb4cf6712c3386654c6689389baf77e35070d0
erik savage <erik.k.savage@gmail.com>
2022-03-09 16:29:01 -0800

    renames tabs changes icons

--- salmon-cookies/src/App.tsx
+++ salmon-cookies/src/App.tsx
@@ -10,10 +10,10 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import { ellipse, home, homeOutline, location, square, statsChart, triangle } from 'ionicons/icons';
+import Home from './pages/Home';
+import Locations from './pages/Locations';
+import SalesLedger from './pages/SalesLedger';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -41,31 +41,31 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
+          <Route exact path="/home">
+            <Home />
           </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
+          <Route exact path="/locations">
+            <Locations />
           </Route>
-          <Route path="/tab3">
-            <Tab3 />
+          <Route path="/salesledger">
+            <SalesLedger />
           </Route>
           <Route exact path="/">
-            <Redirect to="/tab1" />
+            <Redirect to="/home" />
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+          <IonTabButton tab="home" href="/home">
+          <IonIcon icon={home} />
+            <IonLabel>Home</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+          <IonTabButton tab="Locations" href="/locations">
+            <IonIcon icon={location} />
+            <IonLabel>Locations</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+          <IonTabButton tab="salesledger" href="/salesledger">
+            <IonIcon icon={statsChart} />
+            <IonLabel>Sales Ledger</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
--- salmon-cookies/src/pages/SalesLedger.jsx
+++ salmon-cookies/src/pages/SalesLedger.jsx
@@ -1,25 +1,25 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import './SalesLedger.css';
 
-const Tab3 = () => {
+const SalesLedger = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Sales Ledger</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Sales Ledger</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <ExploreContainer name="Sales Ledger" />
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab3;
+export default SalesLedger;
